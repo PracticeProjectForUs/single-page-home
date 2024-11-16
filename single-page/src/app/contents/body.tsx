ea140df138a5c41dfb9df5879b6777f936bb4eaa
chinnnawat <chin3896@gmail.com>
2024-11-16 21:13:36 +0700

    add Info

--- single-page/src/app/contents/body.tsx
+++ single-page/src/app/contents/body.tsx
@@ -1,11 +1,17 @@
-import Backgroud from "./backgroud";
+import SearhBar from "../components/SearhBar";
+import Backgroud from "../components/backgroud";
+import InfoList from "./InfoList";
 
 export default function Body() {
     return (
-        <div>
-            <div>
-                <Backgroud />
+            <div className="relative w-full h-screen">
+                <Backgroud/>
+                <div className="absolute bottom-48 left-0 w-full flex justify-center p-4 z-10">
+                    <SearhBar />
+                </div>
+                <div className="absolute bottom-28 left-0 w-full flex justify-center p-4 z-5">
+                    <InfoList />
+                </div>
             </div>
-        </div>
     )
 }
